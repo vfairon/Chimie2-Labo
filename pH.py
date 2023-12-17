e3d174420346b32e9a089fb5cc82eb6363e49c06
Victor Fairon <67466913+vfairon@users.noreply.github.com>
2023-12-17 16:00:33 +0100

    Add files via upload
    change

--- pH.py
+++ pH.py
@@ -31,48 +31,48 @@ degree_vitesse = 2#MIGHT HAVE TO CHANGE DEGREE HERE WILL SET TWO
 degree_rendement_energetique = 2 #MIGHT HAVE TO CHANGE DEGREE HERE WILL SET TWO
 
 time1 = np.array([1,2,3,4,5]) #[s]
-O_2_pH1 = np.array([1,2,3,4,5]) #[mol/L]
+O_2_pH1 = np.array([1,2,3,4,5]) #[L]
 time1 = np.array([1,2,3,4,5]) #[s]
-H_2_pH1 = np.array([2,4,6,8,10]) #[mol/L]
+H_2_pH1 = np.array([2,4,6,8,10]) #[L]
 
 
 time2 = np.array([1,2,3,4,5]) #[s]
-O_2_pH2 = np.array([-0.5,0,0.5,1,1.5]) #[mol/L]
+O_2_pH2 = np.array([-0.5,0,0.5,1,1.5]) #[L]
 time2 = np.array([1,2,3,4,5]) #[s]
-H_2_pH2 = np.array([-0.69,-0.4,-0.1,0.2,0.5]) #[mol/L]
+H_2_pH2 = np.array([-0.69,-0.4,-0.1,0.2,0.5]) #[L]
 
 
 time3 = np.array([1,2,3,4,5]) #[s]
-O_2_pH3 = np.array([-1,-2,-3,-4,-5]) #[mol/L]
+O_2_pH3 = np.array([-1,-2,-3,-4,-5]) #[L]
 time3 = np.array([1,2,3,4,5]) #[s]
-H_2_pH3 = np.array([-2,-3,-4,-5,-6]) #[mol/L]
+H_2_pH3 = np.array([-2,-3,-4,-5,-6]) #[L]
 
 time4 = np.array([1,2,3,4,5]) #[s]
-O_2_pH4 = np.array([2,3,4,5,6]) #[mol/L]
+O_2_pH4 = np.array([2,3,4,5,6]) #[L]
 time4 = np.array([1,2,3,4,5]) #[s]
-H_2_pH4 = np.array([1.5,2,2.5,3,3.5]) #[mol/L]
+H_2_pH4 = np.array([1.5,2,2.5,3,3.5]) #[L]
 
 
 ####################################### Plot First GRAPH f : t -> [O2], t -> [H2] for different pH level #######################################
 
 ax = plt.subplot(1, 1, 1)
-ax.plot(time1,O_2_pH1, label="[O2] pour pH = " + str(pH1), color="blue")
-ax.plot(time2,O_2_pH2, label="[O2] pour pH =  " + str(pH2),color="blue")
-ax.plot(time3,O_2_pH3, label="[O2] pour pH = "+ str(pH3),color="blue")
-ax.plot(time4,O_2_pH4, label="[O2] pour pH = "+ str(pH4),color="blue")
+ax.plot(time1,O_2_pH1, label="Volume d'O2 pour pH = " + str(pH1), color="blue")
+ax.plot(time2,O_2_pH2, label="Volume d'O2 pour pH =  " + str(pH2),color="blue")
+ax.plot(time3,O_2_pH3, label="Volume d'O2 pour pH = "+ str(pH3),color="blue")
+ax.plot(time4,O_2_pH4, label="Volume d'O2 pour pH = "+ str(pH4),color="blue")
 
 
-ax.plot(time1,H_2_pH1, label="[H2] pour pH =  " + str(pH1) ,color="red")
-ax.plot(time2,H_2_pH2, label="[H2] pour pH =  " + str(pH2),color="red")
-ax.plot(time3,H_2_pH3, label="[H2] pour pH =  " + str(pH3),color="red")
-ax.plot(time4,H_2_pH4, label="[H2] pour pH =  " + str(pH4),color="red")
+ax.plot(time1,H_2_pH1, label="Volume d'H2 pour pH =  " + str(pH1) ,color="red")
+ax.plot(time2,H_2_pH2, label="Volume d'H2 pour pH =  " + str(pH2),color="red")
+ax.plot(time3,H_2_pH3, label="Volume d'H2 pour pH =  " + str(pH3),color="red")
+ax.plot(time4,H_2_pH4, label="Volume d'H2 pour pH =  " + str(pH4),color="red")
     
-plt.title("Variation de [O2] et [H2] pour different pH")
+plt.title("Variation de volume d'H2 et d'O2 pour different pH")
 ax.grid(color='gray',linestyle='dashed') 
 ax.grid(color='gray',linestyle='dashed')
 ax.legend()
 plt.xlabel("Temps [s]")
-plt.ylabel("Concentration [mol/L]")
+plt.ylabel("Volume [L]")
 
 plt.show()
 
@@ -149,10 +149,11 @@ x = np.linspace(pH_levels[0],pH_levels[-1],100)
 ax2 = plt.subplot(1, 1, 1)
 ax2.scatter(pH_levels,speed_H2)
 ax2.scatter(pH_levels,speed_O2)
-ax2.plot(x,polytoplot_H2(x),label="Evolution de la vitesse de [H2] en fonction du pH")
-ax2.plot(x,polytoplot_O2(x),label="Evolution de la vitesse de [O2] en fonction du pH")
+plt.title("Evolution de la vitesse de d'apparition d'H2 et d'O2 en fonction du pH")
+ax2.plot(x,polytoplot_H2(x),label="dV(H2)/dt (pH)")
+ax2.plot(x,polytoplot_O2(x),label="dV(O2)/dt (pH) ")
 plt.xlabel("pH")
-plt.ylabel("Vitesse de concentration [mol/(L*s)]")
+plt.ylabel("Vitesse de concentration [L/(s)]")
 ax2.legend()
 plt.show()
 
@@ -186,8 +187,9 @@ x = np.linspace(pH_levels[0],pH_levels[-1],100)
 ax3 = plt.subplot(1, 1, 1)
 ax3.scatter(pH_levels,evolution_rendement_faradique_H2)
 ax3.scatter(pH_levels,evolution_rendement_faradique_O2)
-ax3.plot(x,polytoplot_rendement_faradique_H2(x),label="Evolution du rendement faradique de [H2] en fonction du pH")
-ax3.plot(x,polytoplot_rendement_faradique_O2(x),label="Evolution du rendement faradique de [O2] en fonction du pH")
+plt.title("Evolution du rendement faradique de H2 et O2 en fonction du pH")
+ax3.plot(x,polytoplot_rendement_faradique_H2(x),label="Rendement faradique de [H2]")
+ax3.plot(x,polytoplot_rendement_faradique_O2(x),label="Rendement faradique de [O2]")
 ax3.legend()
 plt.xlabel("pH")
 plt.ylabel("Rendement Faradique")
@@ -211,6 +213,7 @@ x = np.linspace(pH_levels[0],pH_levels[-1],100)
 ##Plot
 ax4 = plt.subplot(1, 1, 1)
 ax4.scatter(pH_levels,evolution_rendement_energetique)
+
 ax4.plot(x,polytoplot_rendement_energetique(x),label="Evolution du rendement énergétique en fonction du pH")
 ax4.legend()
 plt.xlabel("pH")
